48efde9c9c89102027a7dbe44253450b0d86bdfc
Beab <beabwuletaw@gmail.com>
2024-12-19 14:17:14 -0500

    menu page color

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -4,7 +4,7 @@ export default function Menu() {
   return (
     <div className="h-screen w-screen grid grid-rows-[9rem_1fr_1fr] bg-primary">
       {/* Header */}
-      <div className="bg-brown pt-6 px-2 rounded-b-lg">
+      <div className="bg-secondary pt-6 px-2 rounded-b-lg">
         <div className="flex justify-between pb-2">
           <h1 className="font-semibold text-3xl text-primary text-nowrap">
             Melody
@@ -18,7 +18,7 @@ export default function Menu() {
       </div>
       {/* Menu Categories */}
       <div className="overflow-hidden overflow-y-scroll">
-        <h1 className="text-3xl font-semibold pb-1 text-secondary px-2">Our Lovely Dishes</h1>
+        <h1 className="text-3xl font-semibold pb-1 text-brown px-2">Our Lovely Dishes</h1>
         <div className="flex flex-row gap-4 overflow-x-scroll px-1">
             <RatedMenuCard />
             <RatedMenuCard />
