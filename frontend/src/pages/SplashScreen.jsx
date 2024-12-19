44115f8e4add893772b2c031495f041f00682d49
Beab <beabwuletaw@gmail.com>
2024-12-19 13:21:51 -0500

    splash screen update

--- frontend/src/pages/SplashScreen.jsx
+++ frontend/src/pages/SplashScreen.jsx
@@ -1,12 +1,23 @@
-import { logo } from "../assets"
+import { logo } from "../assets";
 
 export default function SplashScreen() {
   return (
     <div className="bg-primary w-screen h-screen">
-        <div className="flex flex-col items-center">
-            <img src={logo} alt="page_logo" className="w-[10rem] object-contain mt-32" />
-        </div>
+      <div className="flex flex-col items-center">
+        <img
+          src={logo}
+          alt="page_logo"
+          className="w-[10rem] object-contain mt-32 mb-6"
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1 className="text-brown font-semibold text-3xl">
+          Welcome to Melody Cafe
+        </h1>
+        <h2 className="text-lg text-brown">
+            Where Flavor and Harmony Meets!
+        </h2>
+      </div>
     </div>
-  )
+  );
 }
-
