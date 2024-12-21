2dc73e600b141961b166dadc7e706065dd7944f5
amir-mesfin <amir.mesfin136@gmail.com>
2024-12-21 16:46:43 -0300

    Update SplashScreen.jsx

--- frontend/src/pages/SplashScreen.jsx
+++ frontend/src/pages/SplashScreen.jsx
@@ -3,17 +3,27 @@ import { logo } from "../assets";
 export default function SplashScreen() {
   return (
     <div className="bg-primary w-screen h-screen">
-      <div className="flex flex-col items-center">
-        <img
-          src={logo}
-          alt="page_logo"
-          className="w-[10rem] object-contain mt-32 mb-6"
-        />
+        <div className="flex flex-row items-center justify-center mt-32">
+          <div className="border-y-4  flex-row justify-center border-brown" > 
+            <span >
+              <hr className ="w-64 h-4 "/>
+            </span>
+          </div>
+          <img
+            src={logo}
+            alt="page_logo"
+            className="w-[10rem] object-contain mb-6"
+          />
+           <div className="border-y-4  flex-row justify-center border-brown" > 
+            <span >
+              <hr className ="w-64 h-4 "/>
+            </span>
+          </div>
       </div>
       <div className="flex flex-col items-center">
         <h1 className="text-brown font-semibold text-3xl">
           Welcome to Melody Cafe
-        </h1>
+        </h1> 
         <h2 className="text-lg text-brown">
             Where Flavor and Harmony Meets!
         </h2>
