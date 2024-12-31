2a0a09466c40c7cc6f2d6cc3103fd1e2bf27dea3
Beab <beabwuletaw@gmail.com>
2024-12-31 11:53:42 -0500

    doc update

--- backend/models/user.js
+++ backend/models/user.js
@@ -64,7 +64,7 @@ User.prototype.isPasswordCorrect = async function (password) {
     return password === this.password;
 };
 
-// salt the password before it' saved salting 
+// salt the password before it' saved
 User.beforeCreate(async (user) => {
     const salt = await bcrypt.genSalt(5);
     user.password = await bcrypt.hash(user.password, salt);s
