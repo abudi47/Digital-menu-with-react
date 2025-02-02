7545f794b9b6b223d17b4980e4db476ef20bc231
Mr-robot <hackingtest017@gmail.com>
2025-02-02 03:54:42 -0500

    remove fontawsome

--- frontend/src/pages/staff/DashboardLayout.jsx
+++ frontend/src/pages/staff/DashboardLayout.jsx
@@ -4,7 +4,7 @@ export default function DashboardLayout() {
     return (
         <div className="grid grid-cols-[0.2fr_1fr]">
             <div className="border h-screen">
-                <h1>Dashboard</h1>
+                <i className="fa-google text-green-500 text-3xl" /><h1>Dashboard</h1>
             </div>
 
             <div className="border h-screen overflow-y-scroll">
