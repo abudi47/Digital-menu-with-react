c2ae4f6b7797a6755795cd17918bdabaee950398
Mr-robot <hackingtest017@gmail.com>
2025-02-04 10:46:55 -0500

    update table page

--- frontend/src/pages/staff/Table.jsx
+++ frontend/src/pages/staff/Table.jsx
@@ -1,53 +1,72 @@
 import React, { useState } from "react";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import AddPhotoAlternateOutlinedIcon from "@mui/icons-material/AddPhotoAlternateOutlined";
+import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
+import ChevronLeftOutlinedIcon from "@mui/icons-material/ChevronLeftOutlined";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 import SearchField from "../../components/SearchField";
 
 export default function Table() {
     const [expandedRow, setExpandedRow] = useState(null);
+    const [activeTable, setActiveTable] = useState(null);
 
-    const toggleMenu = (id) => {
-        setExpandedRow(expandedRow === id ? null : id);
+    const toggleMenu = (menu) => {
+        setExpandedRow(expandedRow === menu.id ? null : menu.id);
+        setActiveTable(activeTable == menu ? null : menu);
     };
 
-    const orders = [
+    const truncateText = (text, limit = 20) =>
+        text.length > limit ? text.substring(0, limit) + "..." : text;
+
+
+    const tables = [
         {
             id: 1,
-            name: "John Doe",
-            table: "2",
-            orderTime: new Date(),
-            menus: ["Egg", "Toast", "Juice"],
-            status: "Pending",
+            number: "21",
+            category: "normal",
+            price: "5",
+            imageUrl: "https://www.recipetineats.com/tachyon/2014/06/Pasta1.jpg",
+            isAvailable: true,
         },
         {
             id: 2,
-            name: "Jane Smith",
-            table: "12",
-            orderTime: new Date(),
-            menus: ["Pasta", "Salad"],
-            status: "Pending",
+            number: "12",
+            category: "normal",
+            price: "0",
+            imageUrl: "https://www.recipetineats.com/tachyon/2014/06/Pasta1.jpg",
+            isAvailable: true,
         },
         {
             id: 3,
-            name: "Sam Wilson",
-            table: "4",
-            orderTime: new Date(),
-            menus: ["Burger", "Fries", "Soda"],
-            status: "Served",
+            number: "7",
+            category: "normal",
+            price: "15",
+            imageUrl: "https://www.recipetineats.com/tachyon/2014/06/Pasta1.jpg",
+            isAvailable: false,
+        },
+        {
+            id: 4,
+            number: "11",
+            category: "normal",
+            price: "20",
+            imageUrl: "https://www.recipetineats.com/tachyon/2014/06/Pasta1.jpg",
+            isAvailable: true,
         },
     ];
+
     return (
         <div>
             {/* Table Header */}
             <div className="p-4 mb-4 font-bold flex flex-col gap-4">
                 <div className="flex flex-row justify-between items-center">
-                    <h3>New Orders</h3>
+                    <h3>Tables</h3>
 
                     <SearchField />
                 </div>
                 <div className="flex flex-row justify-end">
                     <button className="px-4 py-2 bg-primary text-white font-semibold rounded-md shadow-md hover:bg-primary-600 transition duration-300">
                         <AddOutlinedIcon />
-                        <span className="ml-2">Create Order</span>
+                        <span className="ml-2">Add Table</span>
                     </button>
                 </div>
             </div>
@@ -58,66 +77,166 @@ export default function Table() {
                     <thead className="bg-gray-50 text-gray-600 uppercase text-sm font-semibold">
                         <tr>
                             <th className="p-3">#</th>
-                            <th className="p-3">Name</th>
-                            <th className="p-3">Table</th>
-                            <th className="p-3">Order Time</th>
-                            <th className="p-3 text-center">Menus</th>
+                            <th className="p-3">Number</th>
+                            <th className="p-3">Category</th>
+                            <th className="p-3">Price</th>
                             <th className="p-3 text-center">Status</th>
                         </tr>
                     </thead>
 
                     {/* Table Body */}
-                    <tbody className="text-gray-700">
-                        {orders.map((order) => (
-                            <React.Fragment key={order.id}>
-                                {/* Main Row */}
-                                <tr key={order.id} className="border-t">
-                                    <td className="p-3">{order.id}</td>
-                                    <td className="p-3">{order.name}</td>
-                                    <td className="p-3">{order.table}</td>
+                    <tbody className="text-gray-700 text-nowrap">
+                        {tables.map((table, index) => (
+                            <React.Fragment key={table.id}>
+                                <tr key={index} className="border-t">
+                                    <td className="p-3">{table.id}</td>
+                                    <td
+                                        className="p-3"
+                                        onClick={() => toggleMenu(table)}
+                                    >
+                                        <p title={table.number}>
+                                            {table.number}
+                                        </p>
+                                    </td>
                                     <td className="p-3">
-                                        {order.orderTime.toLocaleTimeString()}
+                                        <p>
+                                            {table.category}
+                                        </p>
                                     </td>
-                                    <td className="p-3 text-center">
-                                        <button
-                                            onClick={() => toggleMenu(order.id)}
-                                            className="text-blue-600 underline"
-                                        >
-                                            {order.menus[0]} ▾{" "}
-                                            {/* Show only the first menu item */}
-                                        </button>
-                                    </td>
+                                    <td className="p-3">{`Br ${table.price}`}</td>
                                     <td className="p-3 text-center">
                                         <span
                                             className={`px-3 py-1 rounded-full text-xs font-semibold
-                        ${
-                            order.status === "Served"
-                                ? "bg-green-100 text-green-700"
-                                : order.status === "Pending"
-                                ? "bg-yellow-100 text-yellow-700"
-                                : "bg-red-100 text-red-700"
-                        }`}
+                      ${
+                          table.isAvailable === true
+                              ? "bg-green-100 text-green-700"
+                              : table.status === false
+                              ? "bg-yellow-100 text-yellow-700"
+                              : "bg-red-100 text-red-700"
+                      }`}
                                         >
-                                            {order.status}
+                                            {String(table.isAvailable)}
                                         </span>
                                     </td>
                                 </tr>
 
                                 {/* Dropdown Row (Pushes Other Rows) */}
-                                {expandedRow === order.id && (
-                                    <tr className="bg-gray-100">
-                                        <td colSpan="6" className="p-3">
-                                            <div className="flex flex-wrap gap-2">
-                                                {order.menus.map(
-                                                    (menu, index) => (
-                                                        <span
-                                                            key={index}
-                                                            className="px-3 py-1 bg-gray-200 text-gray-700 rounded-md text-sm"
-                                                        >
-                                                            {menu}
-                                                        </span>
-                                                    )
-                                                )}
+                                {expandedRow === table.id && (
+                                    <tr className="bg-gray-50/80 hover:bg-gray-100 transition duration-300">
+                                        <td colSpan="6" className="py-6 px-8">
+                                            <div className="flex flex-col md:flex-row gap-6">
+                                                {/* Image section */}
+                                                <div className="flex flex-col gap-3 w-64 md:w-56">
+                                                    <div className="w-full h-56 rounded-lg overflow-hidden shadow-lg border">
+                                                        <img
+                                                            src={
+                                                                table.imageUrl
+                                                            }
+                                                            alt="table_image"
+                                                            className="w-full h-full object-cover"
+                                                        />
+                                                        
+                                                    </div>
+                                                    {/* Image Controls */}
+                                                    <div className="flex justify-between px-3">
+                                                        <AddPhotoAlternateOutlinedIcon className="cursor-pointer text-green-600 hover:text-green-800 transition duration-300" />
+                                                        <DeleteOutlineOutlinedIcon className="cursor-pointer text-red-600 hover:text-red-800 transition duration-300" />
+                                                    </div>
+                                                </div>
+
+                                                {/* Menu Details */}
+                                                <div className="flex flex-col flex-1 gap-4">
+                                                    <div className="w-full max-w-sm min-w-[200px]">
+                                                        <label className="block mb-2 text-sm font-medium text-gray-700">
+                                                            Number
+                                                        </label>
+                                                        <input
+                                                            type="number"
+                                                            className="w-full bg-white border border-gray-300 rounded-lg px-3 py-2 shadow-sm text-gray-700 focus:ring-2 focus:ring-blue-400 focus:outline-none transition duration-300"
+                                                            placeholder="Type menu name here..."
+                                                            value={
+                                                                activeTable.number
+                                                            }
+                                                            onChange={(e) =>
+                                                                setActiveTable(
+                                                                    (prev) => {
+                                                                        return {
+                                                                            ...prev,
+                                                                            number: e
+                                                                                .target
+                                                                                .value,
+                                                                        };
+                                                                    }
+                                                                )
+                                                            }
+                                                        />
+                                                    </div>
+
+                                                    <div className="w-full max-w-sm min-w-[200px]">
+                                                        <label className="block mb-2 text-sm font-medium text-gray-700">
+                                                            Price
+                                                        </label>
+                                                        <input
+                                                            className="w-full bg-white border border-gray-300 rounded-lg px-3 py-2 shadow-sm text-gray-700 focus:ring-2 focus:ring-blue-400 focus:outline-none transition duration-300"
+                                                            placeholder="Type menu price here..."
+                                                            value={
+                                                                activeTable.price
+                                                            }
+                                                            onChange={(e) =>
+                                                                setActiveTable(
+                                                                    (prev) => {
+                                                                        return {
+                                                                            ...prev,
+                                                                            price: e
+                                                                                .target
+                                                                                .value,
+                                                                        };
+                                                                    }
+                                                                )
+                                                            }
+                                                        />
+                                                    </div>
+
+                                                    <div className="w-full max-w-sm min-w-[200px]">
+                                                        <label className="block mb-2 text-sm font-medium text-gray-700">
+                                                            Category
+                                                        </label>
+                                                        <input
+                                                            className="w-full bg-white border border-gray-300 rounded-lg px-3 py-2 shadow-sm text-gray-700 focus:ring-2 focus:ring-blue-400 focus:outline-none transition duration-300"
+                                                            placeholder="Type menu category here..."
+                                                            value={
+                                                                activeTable.category
+                                                            }
+                                                            onChange={(e) =>
+                                                                setActiveTable(
+                                                                    (prev) => {
+                                                                        return {
+                                                                            ...prev,
+                                                                            category:
+                                                                                e
+                                                                                    .target
+                                                                                    .value,
+                                                                        };
+                                                                    }
+                                                                )
+                                                            }
+                                                        />
+                                                    </div>
+                                                </div>
+
+                                                {/* Description & Submit Button */}
+                                                <div className="flex flex-col flex-1 justify-between">
+                                                    <div></div>
+
+                                                    <div className="flex flex-row justify-between">
+                                                        <button className="px-6 py-2 mt-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-600 transition duration-300">
+                                                            Save Changes
+                                                        </button>
+                                                        <button className="px-6 py-2 mt-4 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-600 transition duration-300">
+                                                            Remove Menu
+                                                        </button>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </td>
                                     </tr>
