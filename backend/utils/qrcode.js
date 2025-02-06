bcc032af1a9f1ec7fcbc52acb533b3af9b4cdd31
Mr-robot <hackingtest017@gmail.com>
2025-02-06 07:31:33 -0500

    custom table menu image generator

--- backend/utils/qrcode.js
+++ backend/utils/qrcode.js
@@ -3,9 +3,10 @@
  * @file qrcode.js
  * @description QR code utility functions
  * This module provides utility functions for generating QR codes.
- * 
+ *
  */
 import QRcode from "qrcode";
+import { Jimp } from "jimp";
 
 async function QRcodeGenerator(string) {
     try {
@@ -23,5 +24,15 @@ async function QRcodeGenerator(string) {
         return false;
     }
 }
+
+export async function TableMenuImage(baseImage, qrImage, x, y, outputImage) {
+    const base = await Jimp.read(baseImage);
+    const qr = await Jimp.read(qrImage);
+
+    base.composite(qr, x, y); // Overlay QR code at (x, y)
+    await base.write(outputImage)
+}
+
+TableMenuImage("Pic_for_Product_QR_Menu.png", "foo.png", 300, 260, "output.jpg");
 
 export default QRcodeGenerator;
